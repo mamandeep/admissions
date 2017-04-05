e303d84eb5970a01454e19bc7b2057ccbb5c1827
mamandeep <mann.cse@gmail.com>
2017-04-05 09:11:23 +0530

    Initial commit

---
+++ src/Model/Entity/User.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Model\Entity;
+
+use Cake\Auth\DefaultPasswordHasher;
+use Cake\ORM\Entity;
+
+class User extends Entity
+{
+
+    // Make all fields mass assignable except for primary key field "id".
+    protected $_accessible = [
+        '*' => true,
+        'id' => false
+    ];
+    
+    public $validate = array(
+     'email'=>'email',
+     'password' => array(
+        'required' => array(
+            'rule' => array('notEmpty'),
+            'message' => 'A password is required'
+        )
+     ));
+
+    // ...
+    
+    protected function _setPassword($password)
+    {
+        return (new DefaultPasswordHasher())->hash($password);
+        //return $passwordHasher->hash($password);
+    }
+
+    // ...
+}
---
+++ src/Model/Table/UsersTable.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Model\Table;
+
+use Cake\Validation\Validator;
+use Cake\Auth\DigestAuthenticate;
+use Cake\Auth\DefaultPasswordHasher;
+use Cake\Event\Event;
+use Cake\ORM\Table;
+
+class UsersTable extends Table
+{
+
+    public function validationDefault(Validator $validator)
+    {
+        return $validator
+            ->notEmpty('username', 'A username is required')
+            ->notEmpty('password', 'A password is required')
+            ->notEmpty('role', 'A role is required')
+            ->add('role', 'inList', [
+                'rule' => ['inList', ['admin', 'author']],
+                'message' => 'Please enter a valid role'
+            ]);
+    }
+
+    public function beforeSave($event, $entity, $options)
+    {
+        //debug($event); debug($entity); debug($options);
+        //$alias = $event->subject()->alias();
+        //$users = TableRegistry::get('Users');
+        //if (!empty($entity->get['password'])) {
+            //$entity->set((new DefaultPasswordHasher())->hash($entity->get['password']));
+        //}
+        //else {
+            //debug($event); debug($entity); debug($options);
+            //return false;
+        //}
+        //return true;
+    }
+}
